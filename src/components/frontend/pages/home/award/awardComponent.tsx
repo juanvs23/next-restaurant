cc7a2d3886b37064f9c20e94a2155b040f7c66c1
Juan Carlos Avila Pérez <juanvs23@gmail.com>
2026-06-30 09:46:58 -0400

    i18n: translate Awards section with real content
    - Replace Lorem ipsum trophies with real awards: Michelin Star, Best Restaurant, Golden Cup, Chef of the Year
    - Translate subtitle and title (Awards & Recognition, Our Laurels)

--- src/components/frontend/pages/home/award/awardComponent.tsx
+++ src/components/frontend/pages/home/award/awardComponent.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import logoTop from "@/public/awards/logo-87.svg";
 import awardImga from "@/public/awards/mgg-vitchakorn-J5ZivsKiu9c-unsplash 2.jpg";
@@ -10,14 +11,16 @@ import trophy4 from "@/public/awards/fourth-throphy.svg";
 
 import { SponImage } from "@/components/common";
 
-const trophies = [
-  { src: trophy1, title: "Bib Gourmond", desc: "Lorem ipsum dolor sit amet, consectetur." },
-  { src: trophy2, title: "Bib Gourmond", desc: "Lorem ipsum dolor sit amet, consectetur." },
-  { src: trophy3, title: "Bib Gourmond", desc: "Lorem ipsum dolor sit amet, consectetur." },
-  { src: trophy4, title: "Bib Gourmond", desc: "Lorem ipsum dolor sit amet, consectetur." },
-];
-
 export default function AwardComponent() {
+  const t = useTranslations("awards");
+
+  const trophies = [
+    { src: trophy1, key: "trophy1" },
+    { src: trophy2, key: "trophy2" },
+    { src: trophy3, key: "trophy3" },
+    { src: trophy4, key: "trophy4" },
+  ];
+
   return (
     <section id="awards" className="bg-overlay award-wrapper">
       <div className="container container-award">
@@ -25,16 +28,16 @@ export default function AwardComponent() {
           <div className="element-50">
             <Image src={logoTop.src} height={120} width={120} alt="GERÍCHT restaurant awards logo" />
             <div className="row-award">
-              <h5>Awards & recognition</h5>
+              <h5>{t("subtitle")}</h5>
               <SponImage justify="start" />
-              <h2>Our Laurels</h2>
+              <h2>{t("title")}</h2>
               <div className="row-trophy">
-                {trophies.map((t, i) => (
+                {trophies.map((trophy, i) => (
                   <div key={i} className="trophy">
-                    <Image src={t.src.src} height={100} width={100} alt={`${t.title} award trophy`} />
+                    <Image src={trophy.src.src} height={100} width={100} alt={`${t(`${trophy.key}.name`)} award trophy`} />
                     <div className="text">
-                      <h5>{t.title}</h5>
-                      <p>{t.desc}</p>
+                      <h5>{t(`${trophy.key}.name`)}</h5>
+                      <p>{t(`${trophy.key}.desc`)}</p>
                     </div>
                   </div>
                 ))}
@@ -44,7 +47,7 @@ export default function AwardComponent() {
           <div className="element-50">
             <div className="imageaWard">
               <div className="steak">
-                <Image src={awardImga.src} height={awardImga.height} width={awardImga.width}                  alt="Filete a la parrilla con verduras" />
+                <Image src={awardImga.src} height={awardImga.height} width={awardImga.width} alt="Filete a la parrilla con verduras" />
               </div>
               <div className="gImage">
                 <Image src={G.src} height={G.height} width={G.width} alt="GERÍCHT ornamental letter G" />
